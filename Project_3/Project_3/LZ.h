94cac938f57add89534389f214b08cbbc1394c38
lawdaywa <lawdaywa@gmail.com>
2016-05-31 22:44:36 -0700

    made a branch of vigen's code so I can safely merge it

--- Project_3/Project_3/LZ.h
+++ Project_3/Project_3/LZ.h
@@ -2,25 +2,21 @@
 #define LZ_H
 #include <string>
 #include <iostream>
-#include <cassert>
-#include <map>
-#include <math.h>
-//"using namespace std;" is dangerous, just use what is required for readability
-using std::cout;
-using std::endl;
-using std::string;
-using std::map;
+//#include <map>
+#include <unordered_map>
+using namespace std;
 
 class LZ{
 	private:
 		string wholetext;
 		string window=""; //Stores string that has already been processed
 		string lookahead; //Stores the lookahead buffer
-		map<string, int> dict; //String=actual string or char, int =index of that string
+		unordered_map<string, int> dict; //String=actual string or char, int =index of that string
 	public:
 		LZ(string filename);
 		void setWholetext(string text){ wholetext = text; };
-		void startEncode(int N = 11, int L = 4, int S = 3);
+		void startEncode();
+		void startStEncode();
 		void generatePermutations(string);	//Inserts into the map all the possible combos of matchings, including the new string that was matched
 		void printMap();
 		int calc_num_perms(int,int); //Calculate how many times the for loop in genereatePerm needs to iterate
